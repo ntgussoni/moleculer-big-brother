d5eedc354b0a9e02ff7612feddda353ad558de26
Nicolas Torres <ntorres@ingsw.com>
2017-11-02 20:04:55 -0300

    Adds test services

---
+++ services/n3.service.js
@@ -0,0 +1,63 @@
+"use strict";
+
+module.exports = {
+	name: "n3",
+
+	/**
+	 * Default settings
+	 */
+	settings: {
+
+	},
+
+	/**
+	 * Actions
+	 */
+	actions: {
+
+		/**
+		 * Say a 'Hello'
+		 * 
+		 * @returns 
+		 */
+		ping() {
+			return "PONG";
+		},
+
+	},
+
+	/**
+	 * Events
+	 */
+	events: {
+
+	},
+
+	/**
+	 * Methods
+	 */
+	methods: {
+
+	},
+
+	/**
+	 * Service created lifecycle event handler
+	 */
+	created() {
+
+	},
+
+	/**
+	 * Service started lifecycle event handler
+	 */
+	started() {
+
+	},
+
+	/**
+	 * Service stopped lifecycle event handler
+	 */
+	stopped() {
+
+	}	
+};
